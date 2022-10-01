d6b2de29b08726f7822598a39521476920cf9021
ycleo <leoyclee@gmail.com>
2022-09-30 23:23:14 -0700

    implement sidebar detail

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,12 @@
-$( function() {
+var fileName = document.location.href.match(/[^\/]+$/)[0];
 
+$( function() {
+    $( '.sidebar-container' ).load( 'sidebar.html', function() {
+
+        $( 'a[href="' + fileName + '"] div').addClass( 'nav-selected' );
+
+        $( '.mobile-menu' ).on( 'click', function() {
+            $( '.sidebar-nav-container' ).toggle();
+        });
+    });
 } );
