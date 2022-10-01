325bbf407fbfb67577823b979fdf69c647843b88
ycleo <leoyclee@gmail.com>
2022-10-01 16:44:14 -0700

    fix mobile view sidebar issue

--- javascripts/portfolio.js
+++ javascripts/portfolio.js
@@ -1,19 +1,11 @@
-$(function(){
-	$("#sidebar").load("sidebar.html"); 
-  });
 
-var fileName = document.location.href.match(/[^\/]+$/)[0];
-
-$(document).$( function() { 
-	$( '.sidebar-container' ).load( 'sidebar.html', function() { // load the sidebar html
-		// update selected nav
-		$( 'a[href="' + fileName + '"] div').addClass( 'nav-selected' );
-
-		$( '.mobile-menu' ).on( 'click', function() { // on click for mobile menu bars icon
-			// toggle the navigation
+$(document).ready(function() {
+	$(".sidebar-container").load("sidebar.html", function() {
+		$( '.mobile-menu' ).click(function() {
 			$( '.sidebar-nav-container' ).toggle();
 		} );
-	} );
-} );
+	}); 
+});
+
 
 
